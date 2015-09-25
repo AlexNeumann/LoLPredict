8439fa8e51b4c07154e78bab14b9bfe8a207c083
AlexNeumann <alex.neumann92@gmail.com>
2015-09-25 14:39:13 -0700

    Moved from MatchHistory endpoint to MatchList endpoint

--- Python Scripts/findLeagues.py
+++ Python Scripts/findLeagues.py
@@ -8,21 +8,24 @@ import random
 import datetime
 
 # Riot API key -
-APIkey = 'API_KEY_HERE'
+APIkey = ''
 
 # Summoner ID of a player in the requested skill level (Diamond, Plat, Gold, Silver...)
-# Summoner name = smiling, ID = 20672928
+# Summoner name = smiling, ID = 20672928, tier = Diamond
 nextID = 20672928
 tier = "DIAMOND"
 
-# number of leagues you want to find (avg. division player size = 200)
+# number of leagues you want to find (max division player size = 250)
 league_num = 40
 
 leagueNames = []
 leaguePlayerID = []
 leagueDivision = []
 
+# stats for console output
+totalPlayersFound = 0
 leaguesFound = 0
+matchesLookedAt = 0
 
 # create a new CSV file that will store the league information
 output = open("DiamondLeagues1.csv","w")
@@ -37,18 +40,25 @@ try:
 			playerName = []
 			playerID = []
 			
-			# generate an int between 1 and 5 to randomize which game from the history will be used
+			# generate an int between 1 and 8 to randomize which game from the history will be used
 			# this avoids the 'spidertrap' issue
-			mh_number = random.randint(0,8)
+			mh_number = random.randint(1,8)
 			mh_number1 = mh_number + 1
+			
 			# get the next players match history
-			playerHistory = requests.get("https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/" + str(nextID) + "?rankedQueues=RANKED_SOLO_5x5&beginIndex=" + str(mh_number) + "&endIndex=" + str(mh_number1) + "&api_key=" + APIkey)
+			base_url = "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/"
+			parameter = "?rankedQueues=RANKED_SOLO_5x5&seasons=SEASON2015&beginIndex="		
+			
+			playerHistory = requests.get(base_url + str(nextID) + parameter + str(mh_number) + "&endIndex=" + str(mh_number1) + "&api_key=" + APIkey)
 			playerHistory_data = json.loads(playerHistory.content.decode('utf-8'))					
 			
 			# get the match ID and then use it to call the MatchAPI to find the 10 players in that match
 			matchID = playerHistory_data["matches"][0]["matchId"]
 			match = requests.get("https://na.api.pvp.net/api/lol/na/v2.2/match/" + str(matchID) + "?api_key=" + APIkey)
 			match_data = json.loads(match.content.decode('utf-8'))
+			
+			# add 1 to the number of matches we have pulled
+			matchesLookedAt = matchesLookedAt + 1
 			
 			# loop through the match participants and store their names and IDs
 			for a in range (0,10):
@@ -74,11 +84,12 @@ try:
 					leagueDivision.append(playerLeague_data[str(playerID[b])][0]["tier"])
 					
 					numberofplayers = len(playerLeague_data[str(playerID[b])][0]["entries"])
+					totalPlayersFound = totalPlayersFound + numberofplayers
 					
 					leaguesFound = leaguesFound + 1
-					print("I found a total of: " + str(leaguesFound) + " leagues..." + "Size: " + str(numberofplayers))
+					print("# Leagues Found: " + str(leaguesFound) + "   League Size: " + str(numberofplayers) + "   Total Players: " + str(totalPlayersFound) + "   # Matches Pulled: " + str(matchesLookedAt))
 		except:
-			print("error")
+			print("Error caught, skipping iteration.")
 # allows me to interrupt the script with CTRL-C to break the while loop and write the data to the file
 except KeyboardInterrupt:
 	pass
